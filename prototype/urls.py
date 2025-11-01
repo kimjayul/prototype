d2a73cda7006b5afd52a64475685ee246e748410
rp1028 <lkw20001028@gmail.com>
2025-11-02 03:40:37 +0900

    Connect mypage URLs to main routing

--- prototype/urls.py
+++ prototype/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),  # accounts 앱 분리
     path('api/posts/', include('posts.urls')),        # posts 앱 분리
+    path('api/', include('mypage.urls')), # mypage 추가
 ]
