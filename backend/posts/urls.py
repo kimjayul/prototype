f4b2ddaf004c0bf354c47747994315dff2db6019
rp1028 <lkw20001028@gmail.com>
2025-11-16 16:09:26 +0900

    Remove duplicate backend folder

--- backend/posts/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import PostCreateView, PostListView
-
-urlpatterns = [
-    path('create-post/', PostCreateView.as_view(), name='create-post'),
-    path('list-posts/', PostListView.as_view(), name='list-posts'),
-]
